2de75b987886807034ae6e277a5924ba131be959
Malcolm S. Britton <malcolm.s.britton@gmail.com>
2018-12-09 15:36:50 -0500

    added doc strings and comments to report code

--- loganalyzer/__main__.py
+++ loganalyzer/__main__.py
@@ -1,17 +1,24 @@
+"""Report log analysis to standard out"""
 import psycopg2
 from . import queries
 
-ARTICLE_REPORT_DESC = '1. What are the most popular three articles of all time?'
-ARTICLE_REPORT_FMT = '"{text}" — {amount} views'
-AUTHOR_REPORT_DESC = '2. Who are the most popular article authors of all time?'
-AUTHOR_REPORT_FMT = '"{text}" — {amount} views'
-ERRORS_REPORT_DESC = (
+# Reporting text
+_ARTICLE_REPORT_DESC = '1. What are the most popular three articles of all time?'
+_ARTICLE_REPORT_FMT = '"{text}" — {amount} views'
+_AUTHOR_REPORT_DESC = '2. Who are the most popular article authors of all time?'
+_AUTHOR_REPORT_FMT = '"{text}" — {amount} views'
+_ERRORS_REPORT_DESC = (
     '3. On which days did more than 1% of requests lead to errors?'
 )
-ERRORS_REPORT_FMT = '"{text}" — {amount:.1%} errors'
+_ERRORS_REPORT_FMT = '"{text}" — {amount:.1%} errors'
 
 class Report:
+    """Log analysis report object, manages view creation and report execution
 
+    Report runs at instantiation time. It establishes db connection, creates
+    reporting views from .sql files in `queries` submodule, and executes
+    the three quired reports.
+    """
     def __init__(self):
 
         self.db = psycopg2.connect('dbname=news')
@@ -19,37 +26,50 @@ class Report:
         self.create_view_for_article_views()
         self.report_from_query(
             query=queries.popular_articles,
-            desc=ARTICLE_REPORT_DESC,
-            fmt=ARTICLE_REPORT_FMT
+            desc=_ARTICLE_REPORT_DESC,
+            fmt=_ARTICLE_REPORT_FMT
         )
         self.report_from_query(
             query=queries.author_views,
-            desc=AUTHOR_REPORT_DESC,
-            fmt=AUTHOR_REPORT_FMT
+            desc=_AUTHOR_REPORT_DESC,
+            fmt=_AUTHOR_REPORT_FMT
         )
         self.report_from_query(
             query=queries.errors_by_day,
-            desc=ERRORS_REPORT_DESC,
-            fmt=AUTHOR_REPORT_FMT
+            desc=_ERRORS_REPORT_DESC,
+            fmt=_ERRORS_REPORT_FMT
         )
 
     def create_view_for_article_views(self):
+        """Create view-count-by-slug view from .sql file"""
 
         self.cur.execute(queries.article_views)
         self.db.commit()
 
     def report_from_query(self, query, desc, fmt):
+        """Execute individual report component
 
+        Print report description, execution report sql, and
+        print report output.
+
+        Arguments:
+            query (str): SQL query to generate reporting results
+            desc (str): Report description / header
+            fmt (str): Report output template to be populated 
+                with query results
+        """
+
+        # Print report descrition / header to screen
         print(desc)
+        # Execute analysis query to generate reporting results
         self.cur.execute(query)
+        # Iterate over query results to populate report output
         for text, amount in self.cur.fetchall():
             report_line = fmt.format(text=text, amount=amount)
+            # Print report output to screen
             print(report_line)
 
-    @classmethod
-    def generate_report(cls):
-        cls()
 
 if __name__ == '__main__':
 
-    Report.generate_report()
+    Report()
